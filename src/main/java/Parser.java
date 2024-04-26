e2163b06d90db46d6db86256de6df1d519bfac71
AmirPaknahad <ar.pknhd@gmail.com>
2024-04-26 20:12:18 +0330

    done

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -12,34 +12,42 @@ public class Parser {
     public List<Country> sortByName(){
         List<Country> sortedByName = new ArrayList<>(countries);
         // Sort countries alphabetically (least)
-        //TODO
-        return  sortedByName;
+        Collections.sort(sortedByName,Comparator.comparing(Country::getName));
+        return sortedByName;
     }
 
     public List<Country> sortByPopulation(){
         List<Country> sortedByPopulation = new ArrayList<>(countries);
         // Sort countries by population (most)
-        //TODO
+        Collections.sort(sortedByPopulation , Comparator.comparing(Country::getPopulation).reversed());
         return sortedByPopulation;
     }
 
     public List<Country> sortByArea(){
         List<Country> sortedByArea = new ArrayList<>(countries);
         // Sort countries by area (most)
-        //TODO
+        Collections.sort(sortedByArea , Comparator.comparing(Country::getArea).reversed());
         return sortedByArea;
     }
 
     public void setUp() throws IOException {
 
         //Parse the HTML file using Jsoup
-        //TODO
+        File htmlFile = new File("src/Resources/country-list.html");
+        Document htmlDoc = Jsoup.parse(htmlFile, null);
 
         // Extract data from the HTML
-        //TODO
+        Elements divs = htmlDoc.selectFirst("section#countries").select("div.col-md-4.country");
 
         // Iterate through each country div to extract country data
-        //TODO
+        for (Element div : divs) {
+            String name = div.select(".country-name").text();
+            String capital = div.select(".country-capital").text();
+            int population = Integer.parseInt(div.select(".country-population").text());
+            double area = Double.parseDouble(div.select(".country-area").text());
+            Country country = new Country(name,capital,population,area);
+            countries.add(country);
+        }
     }
 
     public static void main(String[] args) {
